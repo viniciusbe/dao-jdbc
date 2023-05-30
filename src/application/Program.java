cffe4733095d562efcca982c893295b6a9a07333
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-05-30 17:07:23 -0300

    ref: improve SellerDaoJDBC readability and reusability

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,7 @@ public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("------ FIND SELLER BY ID");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
